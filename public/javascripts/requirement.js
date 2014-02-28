2fab88131bf01c0fcb802f4253b08c49d8732fb7
Johannes Unterstein <unterstein@me.com>
2014-02-28 20:50:34 +0100

    edit modal

--- public/javascripts/requirement.js
+++ public/javascripts/requirement.js
@@ -1,21 +1,31 @@
 $(function () {
   $("#newProject").click(function () {
-    $("#newModal").modal("show");
+    $("#projectModal").modal("show");
+    $("#newProjectAdd").show();
     return false;
   });
   /** clear behavior */
-  $("#newModal").on("show.bs.modal", function () {
+  $("#projectModal").on("show.bs.modal", function () {
+    hideAll();
     $(this).find(":input").val("");
   });
+  /** add/edit buttons in modals */
   $("#newProjectAdd").click(function () {
-    var name = $("#newProjectName").val();
-    var description = $("#newProjectDescription").val();
+    var name = $("#projectName").val();
+    var description = $("#projectDescription").val();
     ajaxCall(jsRoutes.controllers.RequirementController.addProject(name, description));
   });
+  hideAll();
   window.pEdit = function (id) {
-
+    $("#projectModal").modal("show");
+    $("#projectEdit").show();
   }
   window.pDelete = function (id) {
 
+  }
+
+  function hideAll() {
+    $("#newProjectAdd").hide();
+    $("#projectEdit").hide();
   }
 });
