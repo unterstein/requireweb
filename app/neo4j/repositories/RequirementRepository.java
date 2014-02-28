1ea75d36662041965908b5f5f5beb3be044d1ed9
Johannes Unterstein <unterstein@me.com>
2014-02-28 21:15:20 +0100

    list main requirements

--- app/neo4j/repositories/RequirementRepository.java
+++ app/neo4j/repositories/RequirementRepository.java
@@ -1,8 +1,15 @@
 package neo4j.repositories;
 
+import neo4j.models.require.Project;
 import neo4j.models.require.Requirement;
+import neo4j.relations.Relations;
+import org.springframework.data.neo4j.annotation.Query;
 import org.springframework.data.neo4j.repository.GraphRepository;
+
+import java.util.List;
 
 public interface RequirementRepository extends GraphRepository<Requirement> {
 
+  @Query("START project=node({0}) MATCH project<-[:" + Relations.PROJECT_REQUIREMENT + "]-requirement WHERE requirement.parent = NULL RETURN requirement")
+  public List<Requirement> findMainRequirementsByProject(Project project);
 }
