d110858fc2dbc1ff39a73a95b9e3f8e9bd3e08df
Johannes Unterstein <unterstein@me.com>
2014-02-28 08:17:17 +0100

    clear behavior

--- public/javascripts/requirement.js
+++ public/javascripts/requirement.js
@@ -2,5 +2,8 @@ $(function() {
   $("#newProject").click(function() {
     $("#newModal").modal("show");
     return false;
-  })
+  });
+  $("#newModal").on("show.bs.modal", function () {
+    $(this).find(":input").val("");
+  });
 });
